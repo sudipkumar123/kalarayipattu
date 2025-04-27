288781f7ad75efecf2e2a833d6bcd27defb4f986
sudip kumar <140592540+sudipkumar123@users.noreply.github.com>
2025-04-27 20:45:46 +0530

    Update settings.py

--- bineesh/bineesh/settings.py
+++ bineesh/bineesh/settings.py
@@ -92,5 +92,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ssudipkumar358@gmail.com'
-EMAIL_HOST_PASSWORD = 'lxds ozpk nzjl kgpb'
+EMAIL_HOST_PASSWORD = 'otre gqga pirx kssfS'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
